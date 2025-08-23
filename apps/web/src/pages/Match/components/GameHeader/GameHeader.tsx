930ab65bcf884955de3e12c1c842fe328a087e8e
Hannah McDonald <hannahnmcdonald@protonmail.com>
2025-08-22 22:00:20 -0400

    upgrades to match start page styling + data rendered

--- apps/web/src/pages/Match/components/GameHeader/GameHeader.tsx
+++ apps/web/src/pages/Match/components/GameHeader/GameHeader.tsx
@@ -1,25 +1,36 @@
 import React from "react";
-import { XStack, Image, Text } from "tamagui";
+import { XStack, Image, Text, YStack } from "tamagui";
+import { GameImage } from "../../../Game/components";
+import { Space } from "@tamagui/lucide-icons";
 
 type GameHeaderProps = {
-    game: {
-        name: string;
-        thumbnail?: string | null;
-    };
+  game: {
+    name: string;
+    thumbnail?: string | null;
+    minPlayers: number;
+    maxPlayers: number;
+    playingTime?: string | null;
+  };
 };
 
 export default function GameHeader({ game }: GameHeaderProps) {
-    return (
-        <XStack ai="center" gap="$3">
-            <Image
-                src={game?.thumbnail || ""}
-                width={80}
-                aspectRatio={4 / 3}
-                borderRadius="$2"
-            />
-            <Text fontSize="$6" fontWeight="700">
-                {game?.name}
-            </Text>
-        </XStack>
-    );
+  console.log("GameHeader game:", game);
+  return (
+    <XStack ai="center" gap="$3">
+      <GameImage src={game.thumbnail || undefined} rounded={true} />
+      <YStack width="70%" jc="space-between" gap="$4">
+        <Text fontSize="$4" fontWeight="700">
+          {game?.name}
+        </Text>
+        <Text fontSize="0.8rem">
+          {`Players: ${game.minPlayers} - ${game.maxPlayers}`}
+        </Text>
+        {game.playingTime && (
+          <Text fontSize="0.8rem" fontWeight="700">
+            {`Playing Time: ${game.playingTime}`}
+          </Text>
+        )}
+      </YStack>
+    </XStack>
+  );
 }
